b1813078062bead8882e0978ad3990c81523593b
Tim Eccleston <teccleston@axialhealthcare.com>
2018-04-14 15:56:35 -0500

    copy react-modal minimal example - https://github.com/reactjs/react-modal#demos

---
+++ disclaimer/disclaimer_modal.js
@@ -0,0 +1,37 @@
+class ExampleApp extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      showModal: false
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal () {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal () {
+    this.setState({ showModal: false });
+  }
+
+  render () {
+    return (
+      <div>
+        <button onClick={this.handleOpenModal}>Trigger Modal</button>
+        <ReactModal
+           isOpen={this.state.showModal}
+           contentLabel="Minimal Modal Example"
+        >
+          <button onClick={this.handleCloseModal}>Accept</button>
+        </ReactModal>
+      </div>
+    );
+  }
+}
+
+const props = {};
+
+ReactDOM.render(<ExampleApp {...props} />, document.getElementById('main'))
