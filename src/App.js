cbcb29a8c83bc5d7cd0e8298f02ffbf78b29b8e0
Max Shenfield <shenfieldmax@gmail.com>
2018-04-14 16:02:10 -0500

    Remove App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
 
 import { Home, PoAForm } from './components';
 
