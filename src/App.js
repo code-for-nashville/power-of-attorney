9cdd88931325bea14d2f0b08b9f1245119d0d91a
Corey <coreyarice@gmail.com>
2018-04-14 14:49:25 -0500

    Complete form without styles

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <nav class="navbar navbar-default">
+          <nav className="navbar navbar-default">
             <div className="container-fluid">
               <div className="navbar-header">
                 <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
--- src/components/poa-form/index.js
+++ src/components/poa-form/index.js
@@ -1,5 +1,10 @@
 import * as React from 'react'
 import styles from './styles'
+import { address } from 'ip';
+
+const MOTHER_ADDRESS = 'motherAddress'
+const FATHER_ADDRESS = 'fatherAddress'
+const CAREGIVER_ADDRESS = 'caregiverAddress'
 
 class PoAForm extends React.Component {
   static navigationOptions = ({ navigation }) => ({
@@ -11,19 +16,19 @@ class PoAForm extends React.Component {
     this.state = {
       numberOfChildren: 1,
       childrenNames: [],
-      motherAddress: {
+      [MOTHER_ADDRESS]: {
         street_address: '',
         locality: '',
         region: '',
         postal_code: '',
       },
-      fatherAddress: {
+      [FATHER_ADDRESS]: {
         street_address: '',
         locality: '',
         region: '',
         postal_code: '',
       },
-      caregiverAddress: {
+      [CAREGIVER_ADDRESS]: {
         street_address: '',
         locality: '',
         region: '',
@@ -43,7 +48,24 @@ class PoAForm extends React.Component {
 
   updateAddress = (e) => {
     const inputName = e.target.name
+    const addressType = e.target.dataset.addressType
+    const value = e.target.value
+    this.setState({
+      [inputName]: {
+        ...this.state[inputName],
+        [addressType]: value,
+      }
+    })
 
+  }
+
+  updateParentalStatus = (e) => {
+    console.dir(e.target)
+  }
+
+  updateParentalStatusText = (e) => {
+    const reason = e.target.value
+    this.setState({ reason })
   }
 
   renderChildrenInputs = () => {
@@ -67,20 +89,57 @@ class PoAForm extends React.Component {
   renderAddress = (name) => {
     return (
       <div>
-        <input onChange={this.updateAddress} type="text" className="form-control" name={name} data-address-type={'street_address'} placeholder="street_address" aria-describedby="sizing-addon1" />
-        <input onChange={this.updateAddress} type="text" className="form-control" name={name} data-address-type={'locality'} placeholder="locality" aria-describedby="sizing-addon1" />
-        <input onChange={this.updateAddress} type="text" className="form-control" name={name} data-address-type={'region'} placeholder="region" aria-describedby="sizing-addon1" />
-        <input onChange={this.updateAddress} type="text" className="form-control" name={name} data-address-type={'postal_code'} placeholder="postal_code" aria-describedby="sizing-addon1" />
+        <input
+          onChange={this.updateAddress}
+          value={this.state[name].street_address}
+          type="text"
+          className="form-control"
+          name={name}
+          data-address-type={'street_address'}
+          placeholder="street_address"
+          aria-describedby="sizing-addon1"
+        />
+        <input
+          onChange={this.updateAddress}
+          value={this.state[name].locality}
+          type="text"
+          className="form-control"
+          name={name}
+          data-address-type={'locality'}
+          placeholder="locality"
+          aria-describedby="sizing-addon1"
+        />
+        <input
+          onChange={this.updateAddress}
+          value={this.state[name].region}
+          type="text"
+          className="form-control"
+          name={name}
+          data-address-type={'region'}
+          placeholder="region"
+          aria-describedby="sizing-addon1"
+        />
+        <input
+          onChange={this.updateAddress}
+          value={this.state[name].postal_code}
+          type="text"
+          className="form-control"
+          name={name}
+          data-address-type={'postal_code'}
+          placeholder="postal_code"
+          aria-describedby="sizing-addon1"
+        />
       </div>
     )
   }
 
   render() {
+    console.log(this.state)
     const pluralizeChild = this.state.numberOfChildren > 1 ? 'Children' : 'Child'
     return (
       <div>
         <h1>POWER OF ATTORNEY FOR CARE OF A MINOR CHILD</h1>
-        <p>1of 5POWER OF ATTORNEY FOR CARE OF A MINOR CHILDUse  of  this  form  is  authorized  by  T.C.A.  §  34-6-301  et  seq.    Completion  of  this  form, along with the proper signatures, is sufficient to authorize enrollment of a minor in school and  to  authorize  medical  treatment.    However,  a  school  district  may  require  additional documentation/information as permitted by this section of Tennessee law before enrolling a child in school or any extracurricular activities.</p>
+        <p>Use  of  this  form  is  authorized  by  T.C.A.  §  34-6-301  et  seq.    Completion  of  this  form, along with the proper signatures, is sufficient to authorize enrollment of a minor in school and  to  authorize  medical  treatment.    However,  a  school  district  may  require  additional documentation/information as permitted by this section of Tennessee law before enrolling a child in school or any extracurricular activities.</p>
         <p><strong>Part I:</strong>  To be filled out and/or initialed by parent(s)/legal guardian(s)</p>
         <div className="btn-group">
           <button className="btn dropdown-toggle" data-toggle="dropdown">{`${this.state.numberOfChildren} ${pluralizeChild}`}</button>
@@ -99,16 +158,53 @@ class PoAForm extends React.Component {
           {this.renderChildrenInputs()}
         </div>
         <div className="row">
-          <p>2. Mother/Legal Guardian’s Name & Address</p> {this.renderAddress()}
+          <p>2. Mother/Legal Guardian’s Name & Address</p> {this.renderAddress(MOTHER_ADDRESS)}
         </div>
         <div className="row">
-          <p>3. Father/Legal Guardian’s Name & Address</p> {this.renderAddress()}
+          <p>3. Father/Legal Guardian’s Name & Address</p> {this.renderAddress(FATHER_ADDRESS)}
         </div>
         <div className="row">
-          <p>4. Caregiver’s Name & Address</p> {this.renderAddress()}
+          <p>4. Caregiver’s Name & Address</p> {this.renderAddress(CAREGIVER_ADDRESS)}
         </div>
         <div>
-          <p> 5. Both  parents  are  living,have  legal  custody  of  the  minor  child  and  have signed this document;OR(____)One parent is deceased;OR(____)One  parent  has  legal  custody  of  the  minor  child  and  both  parents  have signed this document and consent to the appointment of the caregiver;OR(____) One parent has legal custody of the minor child, and has sent by Certified Mail, Return Receipt requested, to the other parent at last known address, a  copy  of  this  document  and  a  notice  of  the  provisions  in  §  34-6-305;  or the non-custodial parent has not consented to the appointment and consent cannot be obtained because ______________________________</p>
+          <p> 5.</p>
+          <div clasName="input-group">
+            <div clasName='row'>
+              <span clasName="input-group-addon">
+                <input type="radio" onChange={this.updateParentalStatus} value={0} name='parental_status' aria-label='parental_status' />Both  parents  are  living,have  legal  custody  of  the  minor  child  and  have signed this document;
+                </span>
+            </div>
+            <div clasName='row'>
+              <span clasName="input-group-addon">
+                <input type="radio" onChange={this.updateParentalStatus} value={1} name='parental_status' aria-label='parental_status' />One parent is deceased;
+              </span>
+            </div>
+            <div clasName='row'>
+              <span clasName="input-group-addon">
+                <input type="radio" onChange={this.updateParentalStatus} value={2} name='parental_status' aria-label='parental_status' />
+                One  parent  has  legal  custody  of  the  minor  child  and  both  parents  have signed this document and consent to the appointment of the caregiver;
+              </span>
+            </div>
+            <div clasName='row'>
+              <span clasName="input-group-addon">
+                <input type="radio" onChange={this.updateParentalStatus} value={3} name='parental_status' aria-label='parental_status' /> One parent has legal custody of the minor child, and has sent by Certified Mail, Return Receipt requested, to the other parent at last known address, a  copy  of  this  document  and  a  notice  of  the  provisions  in  §  34-6-305;
+          </span>
+            </div>
+            <div clasName='row'>
+              <span clasName="input-group-addon">
+                <input type="radio" onChange={this.updateParentalStatus} value={4} name='parental_status' aria-label='parental_status' />
+                or the non-custodial parent has not consented to the appointment and consent cannot be obtained because
+                <input
+                  onChange={this.updateParentalStatusText}
+                  value={this.state.reason}
+                  type="text"
+                  className="form-control"
+                  name={'parent_status_reason'}
+                  aria-describedby="sizing-addon1"
+                />
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     )
