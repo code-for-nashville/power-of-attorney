3209d68f452262e0cca2abc54daf00d5b1e72eed
Max Shenfield <shenfieldmax@gmail.com>
2018-04-14 15:43:15 -0500

    Change button text

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -15,7 +15,7 @@ export default () => {
             Immigrant parents may face detainment or deportation with little warning. Since parents may work in the same location or be in the same location when picked up by ICE/immigration, their children can be left without any legal guardian. When picked up by ICE/Immigration, parents are often unavailable or inaccessible for days or weeks (and can even be deported before ever having access to their children or family). Parents can sign a power of attorney for the minor children to give guardianship of their child to someone they trust, which takes effect only if they are detained or deported.
           </p>
           <Link className="btn btn-lg btn-success" role="button" to="/form">
-            Start Filling Out Your Power of Attorney
+            Fill Out Your Power of Attorney Form Now
           </Link>
         </section>
         <section>
