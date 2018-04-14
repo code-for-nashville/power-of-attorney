b19b2ee056d5362f71c3c5bac017f0a9a280aa66
Max Shenfield <shenfieldmax@gmail.com>
2018-04-14 14:20:03 -0500

    Some extra styling for the nav

--- src/App.js
+++ src/App.js
@@ -11,27 +11,15 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <nav class="navbar navbar-default">
+          <nav className="navbar navbar-default">
             <div className="container-fluid">
               <div className="navbar-header">
-                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="#">Power of Attorney</a>
+                <Link className="navbar-brand" to="/">Power of Attorney</Link>
               </div>
-
-              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav">
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/form">Form</Link>
-                  </li>
-                </ul>
+              <div className="navbar-right">
+                <Link role="button" className="btn btn-info" to="/form">
+                  Start Filling Out Your Power of Attorney
+                </Link>
               </div>
             </div>
           </nav>
