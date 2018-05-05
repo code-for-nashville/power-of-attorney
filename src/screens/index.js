334cd943224263f7f008f994e2dc32d28b01e7e2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-05 16:52:50 +0000

    Deploy code-for-nashville/power-of-attorney to github.com/code-for-nashville/power-of-attorney.git:gh-pages

--- src/screens/index.js
+++
@@ -1,5 +0,0 @@
-import FAQ from './faq'
-import Home from './home'
-import PoAForm from './poa-form'
-
-export {FAQ, Home, PoAForm}
--- src/strings/index.js
+++
@@ -1,3 +0,0 @@
-import STATE_OPTIONS from './states'
-
-export {STATE_OPTIONS}
