448918c41d8847de1dcba5f885488105c2e6ba45
Corey <coreyarice@gmail.com>
2018-07-28 11:20:40 -0500

    Toggle languages

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-
-import {Box, Header, Menu} from 'grommet'
+import i18n from 'i18next'
+import {Box, Button, Header, Menu} from 'grommet'
 import GrommetClassnames from 'grommet/utils/CSSClassnames.js'
 import {translate} from 'react-i18next'
 import {FAQ_PATH, HOME_PATH} from '../../paths'
+
+import './style.css'
 
 // Class added to Grommet anchor when `disabled=True`
 // Below we use NavLink instead of <Anchor tag={NavLink}...> to work around
@@ -13,9 +15,14 @@ const ANCHOR_DISABLED = `${GrommetClassnames.ANCHOR}--disabled`
 
 const POAHeader = props => {
   const {t} = props
+  
+  const nextLanguage = i18n.language === 'en' ? 'es' : 'en'
+  const label = i18n.language === 'en' ? 'Español' : 'English'
+  const onClick = () => i18n.changeLanguage(nextLanguage)
+
   return (
     <Header>
-      <Box flex={true} justify="end" direction="row" responsive={false}>
+      <Box flex={true} justify="between" direction="row" responsive={false}>
         <Menu direction="row">
           <NavLink
             activeClassName={ANCHOR_DISABLED}
@@ -33,7 +40,11 @@ const POAHeader = props => {
           >
             {t('FAQ')}
           </NavLink>
+
         </Menu>
+        <Box>
+        <Button className={'language-button'} label={label} onClick={onClick} secondary  hoverIndicator={{background: 'neutral-2'}}/>
+        </Box>
       </Box>
     </Header>
   )
--- src/strings/i18n.js
+++ src/strings/i18n.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import english from './english'
+import spanish from './spanish'
 
 i18n.use(LanguageDetector).init({
   fallbackLng: 'en',
@@ -11,9 +12,13 @@ i18n.use(LanguageDetector).init({
     bindI18n: 'languageChanged loaded',
     bindStore: 'added removed',
     nsMode: 'default'
+  },
+  detection: {
+    order: [ 'navigator', 'querystring', 'cookie', 'localStorage', 'htmlTag', 'path', 'subdomain'],
   }
 })
 
-i18n.addResourceBundle('en', 'translation', english, true, true)
+i18n.addResourceBundle('en', 'translation', english)
+i18n.addResourceBundle('es', 'translation', spanish)
 
 export default i18n
