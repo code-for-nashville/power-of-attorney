c5214cb18a1cacf962cc740a4ccd9532d540f1c8
Corey <coreyarice@gmail.com>
2018-07-29 11:03:17 -0500

    Abstract language detection

--- src/helpers/i18n.js
+++ src/helpers/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import english from './english'
-import spanish from './spanish'
+import english from '../strings/english'
+import spanish from '../strings/spanish'
 
 i18n.use(LanguageDetector).init({
   fallbackLng: 'en',
@@ -28,5 +28,13 @@ i18n.use(LanguageDetector).init({
 
 i18n.addResourceBundle('en', 'translation', english)
 i18n.addResourceBundle('es', 'translation', spanish)
+
+export const getCurrentLanguage = () => {
+  // English and Spanish are currently the only supported languages
+  if (i18n.language && i18n.language.includes('es')) {
+    return 'es'
+  }
+  return 'en'
+}
 
 export default i18n
