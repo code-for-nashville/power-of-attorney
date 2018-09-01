a140b8982eb92d25166ddcb0f6fd40e61a568053
Corey <coreyarice@gmail.com>
2018-09-01 11:13:17 -0500

    Start form cleanup

--- src/components/async-download-pdf/DownloadPDF.js
+++ src/components/async-download-pdf/DownloadPDF.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-import {Box, Button, Header} from 'grommet'
+import { Box, Button, Header } from 'grommet'
 
 import pdfFonts from 'pdfmake/build/vfs_fonts'
 import pdfMake from 'pdfmake/build/pdfmake'
@@ -10,6 +10,30 @@ import createDocDefinition from '../../pdf/pdf-document'
 import './style.css'
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs
+
+pdfMake.tableLayouts = {
+  underlineLayout: {
+    hLineWidth: function (i, node) {
+      if (i === 0) {
+        return 0;
+      }
+      return 1;
+    },
+    vLineWidth: function (i) {
+      return 0;
+    },
+    hLineColor: function (i) {
+      return 'black';
+    },
+    paddingLeft: function (i) {
+      return 0
+    },
+    paddingRight: function (i, node) {
+      return 0
+    }
+  }
+}
+
 
 export default class DownloadPDF extends Component {
   _downloadPDF = () => {
