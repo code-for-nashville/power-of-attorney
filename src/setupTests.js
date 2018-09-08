dba7e5b5da95596743c8a70bb06d4db5726b7ccd
Corey <coreyarice@gmail.com>
2018-09-08 17:55:16 -0500

    Stub I18nextProvider

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,6 @@
 jest.mock('react-i18next', () => ({
+  __esModule: true,
+  I18nextProvider: (Component) => Component.children,
   // this mock makes sure any components using the translate HoC receive the t function as a prop
   translate: () => Component => {
     Component.defaultProps = {...Component.defaultProps, t: () => ''}
