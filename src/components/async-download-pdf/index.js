9508d43d3a7799083d36ac589c694e54a5f30339
Corey Rice <corey@clearsumm.it>
2019-11-02 15:55:13 -0300

    Fix tests, improve flow

--- src/components/async-download-pdf/index.js
+++ src/components/async-download-pdf/index.js
@@ -1,7 +1,7 @@
 // @flow
-import React from 'react'
+import * as React from 'react'
 import {asyncComponent} from 'react-async-component'
-
+import DownloadPDF from './DownloadPDF'
 import {Spinning, Toast} from 'grommet'
 import {translate} from 'react-i18next'
 
@@ -9,9 +9,11 @@ const ErrorNotification = ({t}) => (
   <Toast status="critical">{t('unknownError')}</Toast>
 )
 
-export default asyncComponent({
+const asyncDownload: typeof DownloadPDF = asyncComponent({
   ErrorComponent: translate()(ErrorNotification),
   LoadingComponent: Spinning,
   name: 'AsyncDownloadPDF',
   resolve: () => import('./DownloadPDF')
 })
+
+export default asyncDownload
