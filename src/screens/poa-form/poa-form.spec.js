d8480126d215c63e9fcf4dd94ad985a77943b3b4
Mitch Spradlin <profoundboat@outlook.com>
2020-06-22 20:06:26 -0500

    Adding general error message on validation failure (#129)
    * Adding general error message on validation failure
    
    * Adding test for general error message

--- src/screens/poa-form/poa-form.spec.js
+++ src/screens/poa-form/poa-form.spec.js
@@ -6,3 +6,13 @@ it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<POAForm />, div)
 })
+
+it('displays an error message when form fields are missing', () => {
+  const div = document.createElement('div')
+  const ref = ReactDOM.render(<POAForm />, div)
+  expect(div.getElementsByClassName("form-error-message").length).toEqual(0)
+
+  ref._next()
+
+  expect(div.getElementsByClassName("form-error-message").length).toEqual(1)
+})
