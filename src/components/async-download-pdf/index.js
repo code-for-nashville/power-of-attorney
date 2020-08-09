dca9f7717213fcb777e198b1dcb7f330e1221ed3
Mitch Spradlin <mitch+GitHub@applicative.us>
2020-08-09 16:04:00 -0500

    Adding Modal for error

--- src/components/async-download-pdf/index.js
+++ src/components/async-download-pdf/index.js
@@ -1,17 +1,38 @@
 // @flow
-import * as React from 'react'
+import React, {useState} from 'react'
 import {asyncComponent} from 'react-async-component'
 import DownloadPDF from './DownloadPDF'
 import {withTranslation} from 'react-i18next'
 import Loader from 'react-loader-spinner'
+import {Box, Layer, Paragraph, Button} from 'grommet'
 import { POA_CORAL_BLUE } from '../../styles/grommet/theme'
 
-// TODO Toast for error
-const ErrorNotification = ({t}) => (
-  <alert>{t('unknownError')}</alert>
-)
+const ErrorNotification = ({t}) => {
+  const [open, setOpen] = useState(true)
+  if (!open) {
+    return null
+  }
+  return (
+    <Layer className='Disclaimer' align="center" margin="medium">
+      <Box overflow="auto" gap="small" width="large">
+        <Box pad={{horizontal: 'medium', vertical: 'medium'}}>
+          <Paragraph margin="small" fill={true}>{t('unknownError')}</Paragraph>
+          <Box
+            fill="horizontal"
+            align="center"
+            direction="row"
+            justify="between"
+            pad={{vertical: 'large', horizontal: 'medium'}}
+          >
+            <Button size="small" primary={true} onClick={() => setOpen(false)}
+              label={t('OK')}/>
+          </Box>
+        </Box>
+      </Box>
+    </Layer>
+  )
+}
 
-// TODO Add spinner back for loading component
 const asyncDownload: typeof DownloadPDF = asyncComponent({
   ErrorComponent: withTranslation()(ErrorNotification),
   LoadingComponent: () =>
